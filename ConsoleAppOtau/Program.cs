4d39fc4ae53f35668cfbd0f512968a98716f6150
mli2805 <mli2805@gmail.com>
2017-03-20 21:15:22 +0300

    read / write ini

--- ConsoleAppOtau/Program.cs
+++ ConsoleAppOtau/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleAppOtau
 {
@@ -29,6 +31,13 @@ namespace ConsoleAppOtau
             else
                 Console.WriteLine($"some error");
 
+
+            if (EraseAdditionalOtauFromIni(serverIp, tcpPort, 1))
+                Console.WriteLine($"detached successfully");
+
+            if (InsertAdditionalOtauToIni(serverIp,tcpPort,1,"192.168.96.57",11834))
+                Console.WriteLine($"detached successfully");
+
             var extendedPorts = GetExtentedPorts(serverIp, tcpPort);
             foreach (var pair in extendedPorts)
                 Console.WriteLine($"otau {pair.Value} is attached to port {pair.Key}");
@@ -55,22 +64,57 @@ namespace ConsoleAppOtau
             return false;
         }
 
+        private static bool InsertAdditionalOtauToIni(string serverIp, int tcpPort, int fromOpticalPort, string addOtauIp, int addOtauTcpPort)
+        {
+            var extPorts = GetExtentedPorts(serverIp, tcpPort);
+            if (extPorts.ContainsKey(fromOpticalPort))
+            {
+                Console.WriteLine("This is extended port already. Denied.");
+                return true;
+            }
+            extPorts.Add(fromOpticalPort, $"{addOtauIp}:{addOtauTcpPort}");
+            var content = DictionaryToContent(extPorts);
+            string answer;
+            return SendWriteIniCommand(serverIp, tcpPort, content, out answer);
+        }
+        private static bool EraseAdditionalOtauFromIni(string serverIp, int tcpPort, int fromOpticalPort)
+        {
+            var extPorts = GetExtentedPorts(serverIp, tcpPort);
+            if (!extPorts.ContainsKey(fromOpticalPort))
+            {
+                Console.WriteLine("There is no such extended port. Nothing to do.");
+                return true;
+            }
+
+            extPorts.Remove(fromOpticalPort);
+            var content = DictionaryToContent(extPorts);
+            string answer;
+            return SendWriteIniCommand(serverIp, tcpPort, content, out answer);
+        }
+
+        private static string DictionaryToContent(Dictionary<int, string> extPorts)
+        {
+            if (extPorts.Count == 0)
+                return "\r\n";
+            var result = "[OpticalPortExtension]\r\n";
+            foreach (var extPort in extPorts)
+                result += $"{extPort.Key}={extPort.Value}\r\n";
+            return result;
+        }
+
         private static Dictionary<int, string> GetExtentedPorts(string serverIp, int tcpPort)
         {
             string iniContent;
-            if (!GetIniFile(serverIp, tcpPort, out iniContent))
+            if (!ReadIniFile(serverIp, tcpPort, out iniContent))
                 return null; // read iniFile error
 
             if (iniContent.Substring(0, 15) == "ERROR_COMMAND\r\n")
-                return new Dictionary<int, string>(); // charon too old, know nothing about extentions
-
-            if (iniContent.Substring(0, 15) == new string((char)255, 15))
-                return new Dictionary<int, string>(); // charon ahs never had ini file, so it hasn't got extentions so far
+                return new Dictionary<int, string>(); // charon too old, know nothing about extensions
 
             if (iniContent.Substring(0, 22) == "[OpticalPortExtension]")
                 return ParseIniContent(iniContent);
 
-            return null; // some unknown answer
+            return new Dictionary<int, string>(); 
         }
 
         private static Dictionary<int, string> ParseIniContent(string content)
@@ -85,12 +129,66 @@ namespace ConsoleAppOtau
             }
             return result;
         }
-        private static bool GetIniFile(string serverIp, int tcpPort, out string content)
+        private static bool ReadIniFile(string serverIp, int tcpPort, out string content)
         {
             string cmd = "ini_read\r\n";
             return SendCommand(serverIp, tcpPort, cmd, out content);
         }
 
+        private static bool SendWriteIniCommand(string serverIp, int tcpPort, string content, out string answer)
+        {
+            string cmd = "ini_write\r\n";
+            try
+            {
+                //---create a TCPClient object at the IP and port no.---
+                TcpClient client = new TcpClient(serverIp, tcpPort);
+                NetworkStream nwStream = client.GetStream();
+
+                //---send the command---
+                byte[] bytesToSend = Encoding.ASCII.GetBytes(cmd);
+                Console.WriteLine("Sending : \n" + cmd);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                //---read back the answer---
+                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                Console.WriteLine("Received : \n" + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+
+                //---send the content---
+                byte[] contentBytes = new byte[480];
+                var contentB = Encoding.ASCII.GetBytes(content);
+                Array.Copy(contentB, contentBytes, contentB.Length);
+                byte[] bytes256 = new byte[256];
+                Array.Copy(contentBytes, bytes256, 256);
+
+                Console.WriteLine("Sending : \n" + bytes256);
+                nwStream.Write(bytes256, 0, bytes256.Length);
+
+                byte[] bytes224 = new byte[224];
+                Array.Copy(contentBytes, 256, bytes224, 0, 224);
+
+                Console.WriteLine("Sending : \n" + bytes224);
+                nwStream.Write(bytes224, 0, bytes224.Length);
+
+                Thread.Sleep(1000);
+
+                //---read back the answer---
+                bytesToRead = new byte[client.ReceiveBufferSize];
+                bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                Console.WriteLine("Received : \n" + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+
+                client.Close();
+                answer = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                answer = e.Message;
+                return false;
+            }
+
+        }
         private static bool SendCommand(string serverIp, int tcpPort, string cmd, out string answer)
         {
             try
