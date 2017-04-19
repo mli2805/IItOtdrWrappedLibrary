4c3f98b06d97faf31da667ce22e9928196eddbdd
mli2805 <mli2805@gmail.com>
2017-04-19 16:45:29 +0300

    DataTable

--- WpfExample/RftsEventsOneLevelViewModel.cs
+++ WpfExample/RftsEventsOneLevelViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Optixsoft.SorExaminer.OtdrDataFormat;
+﻿using System.Data;
+using Optixsoft.SorExaminer.OtdrDataFormat;
 using Optixsoft.SorExaminer.OtdrDataFormat.Structures;
 
 namespace WpfExample
@@ -9,6 +10,8 @@ namespace WpfExample
         private readonly RftsLevel _rftsLevel;
         public string ComparisonResult { get; set; }
 
+        public DataTable BindableTable { get; set; }
+
         public RftsEventsOneLevelEeltViewModel EeltViewModel { get; set; }
 
         public RftsEventsOneLevelViewModel(OtdrDataKnownBlocks sorData, RftsLevel rftsLevel)
@@ -16,9 +19,30 @@ namespace WpfExample
             _sorData = sorData;
             _rftsLevel = rftsLevel;
 
+            CreateTable();
+            PopulateTable();
             EeltViewModel = new RftsEventsOneLevelEeltViewModel(_rftsLevel, _sorData.KeyEvents.EndToEndLoss);
 
             ComparisonResult = $"State: ";
         }
+
+
+        private void PopulateTable()
+        {
+            
+        }
+        private void CreateTable()
+        {
+            BindableTable = new DataTable();
+
+            DataColumn parametersColumn = new DataColumn() { ColumnName = "Parameters" };
+            BindableTable.Columns.Add(parametersColumn);
+
+            for (int i = 0; i < _sorData.RftsEvents.EventsCount; i++)
+            {
+                BindableTable.Columns.Add(new DataColumn($"Event N{i}") { DataType = typeof(string) });
+            }
+        }
+
     }
 }
